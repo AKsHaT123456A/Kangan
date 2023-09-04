342c1ab4a47cc79a0ed2a24cdc693c3add90fd46
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-09-04 22:50:35 +0530

    hd

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			'/api': {
-				target: 'http://localhost:5000',
+				target: 'https://kanban-6kme.onrender.com',
 				changeOrigin: true,
 				secure: false,
 				ws: true,
